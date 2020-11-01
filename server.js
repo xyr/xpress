edad0d2e064da044c0ba58c0557a1d51794c0f14
Mika Eriksson <mika.eriksson@iki.fi>
2020-11-01 09:31:56 +0200

    Did add client updates with socket.io

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -17,7 +19,12 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
   messages.push(req.body)
+  io.emit('message', req.body)
   res.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
+  console.log('user connected')
 })
 
 let port = process.env.PORT;
@@ -25,7 +32,7 @@ if (port == null || port == "") {
     port = 3000;
 }
 
-var server = app.listen(port, () => {
+var server = http.listen(port, () => {
   console.log('server is listening on port', server.address().port)
 })
 
