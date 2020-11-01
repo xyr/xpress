cb4f546d5d8764b1327e2bf423d2dce0c9fffc1f
Mika Eriksson <mika.eriksson@iki.fi>
2020-11-01 10:21:37 +0200

    Mlab Mongodb

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb+srv://user:user@cluster0.0aowe.mongodb.net/nodejs-test?retryWrites=true&w=majority'
+
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
 
 var messages = [
   {name: 'Mika', message: 'Hi'},
@@ -14,17 +22,29 @@ var messages = [
 ]
 
 app.get('/messages', (req, res) => {
-  res.send(messages)
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
 })
 
 app.post('/messages', (req, res) => {
-  messages.push(req.body)
-  io.emit('message', req.body)
-  res.sendStatus(200)
+  var message = new Message(req.body)
+
+  message.save((err) => {
+    if (err)
+      sendStatus(500)
+
+    io.emit('message', req.body)
+    res.sendStatus(200)
+  })
 })
 
 io.on('connection', (socket) => {
   console.log('user connected')
+})
+
+mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+  console.log('mongo db connection', err)
 })
 
 let port = process.env.PORT;
